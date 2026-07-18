b968344e1471982ea61c09b336a3be42544f5cbb
swilly26 <swilly26@users.noreply.github.com>
2026-07-18 18:51:07 +0000

    chore: initial repo scaffold

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -4,6 +4,15 @@ Provides abstract base classes for market data providers and
 standardised data structures for downstream consumers.
 """
 
+from src.data.cache import MarketDataCache
 from src.data.provider import DataProvider, MarketData, MarketDataFrame
+from src.data.validation import clean_market_data, validate_market_data
 
-__all__ = ["DataProvider", "MarketData", "MarketDataFrame"]
+__all__ = [
+    "DataProvider",
+    "MarketData",
+    "MarketDataFrame",
+    "MarketDataCache",
+    "clean_market_data",
+    "validate_market_data",
+]
